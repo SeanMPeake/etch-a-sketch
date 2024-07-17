ea865df168449c1de5ca6c4c6ee43fe24a69550f
SeanMPeake <171083841+SeanMPeake@users.noreply.github.com>
2024-07-17 14:53:18 -0500

    Update grid generation to be a function
    Function has a default value of 16 and runs on page load.

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const container = document.querySelector(".container");
 
 let gridSize = 32;
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+function generateGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-    for (let j = 0; j < gridSize; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", addColor)
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", addColor)
 
-        row.appendChild(square);
-    }
+            row.appendChild(square);
+        }
 
     container.appendChild(row);
-}
+    }
+} 
+
+generateGrid();
 
 function addColor() {
     this.classList.add("mouseover");
